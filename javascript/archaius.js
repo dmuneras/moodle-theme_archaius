95430abaf92196d6c952d75857e6afeac229b442
dmuneras <dmunera119@gmail.com>
2013-03-11 23:34:14 +0100

    adding screenshot

--- javascript/archaius.js
+++ javascript/archaius.js
@@ -253,27 +253,27 @@ $(function(){
     /* --------------------------------------------------------------                             
        Custommenu
         ----------------------------------------------------------------*/
-    //Adding arrow to custom sub menu and binding event                                                                                                                 
-    if($("#custommenu .custom_menu_submenu").length > 0 ){
-        $("#custommenu .custom_menu_submenu").each(function(){
+    //Adding arrow to custom sub menu and binding event 
+    var customMenu = $("#custommenu .custom_menu_submenu");                                                                                                                
+    if(customMenu.length > 0 ){
+        customMenu.find(".custom_menu_submenu").each(function(){
             $this = $(this);
             if($this.parents().length == 8 ){
-            $this.prepend("<div class='arrow-up'></div>");
-            
+                $this.prepend("<div class='arrow-up'></div>");
             }else{
-            $this.prepend("<div class='arrow-right'></div>");
-            $(".custom_menu_submenu .yui3-menu-content li").each(function(){
-                var item = $(this);
-                if(item.find("div.custom_menu_submenu").length > 0){
-                    item.on("hover",function(){
-                        $(this).find("div.custom_menu_submenu .yui3-menu-content").css({
-                            "position" : "absolute" ,
-                            "left" : "8px" ,
-                            "margin-top" : (parseInt($(this).css("height") + 20) * (-1)) + "px"
+                $this.prepend("<div class='arrow-right'></div>");
+                $(".custom_menu_submenu .yui3-menu-content li").each(function(){
+                    var item = $(this);
+                    if(item.find("div.custom_menu_submenu").length > 0){
+                        item.on("hover",function(){
+                            $(this).find("div.custom_menu_submenu .yui3-menu-content").css({
+                                "position" : "absolute" ,
+                                "left" : "8px" ,
+                                "margin-top" : (parseInt($(this).css("height") + 20) * (-1)) + "px"
                             });
                         
-                    });
-                }
+                        });
+                    }
                 });
             }
         });
