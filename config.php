d3fb5d30b4b297f77a4058477ccb84ca58613877
dmuneras <dmunera119@gmail.com>
2013-12-29 17:59:36 -0500

    Adding GPL license comments

--- config.php
+++ config.php
@@ -1,5 +1,22 @@
 <?php
 
+/*  
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+This plugin is part of Archaius theme.
+@copyright  2013 Daniel Munera Sanchez
+
+*/
+
 ////////////////////////////////////////////////////////////////////////////////
 /// This file contains a few configuration variables that control
 /// how Moodle uses this theme.
--- slideshow/models/Slider.php
+++ slideshow/models/Slider.php
@@ -1,4 +1,20 @@
 <?php
+/*  
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+This plugin is part of Archaius theme.
+@copyright  2013 Daniel Munera Sanchez
+
+*/
 
 /**
 * Archaius slider from page slider
